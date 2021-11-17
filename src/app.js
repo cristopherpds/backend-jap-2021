96a49b1b00ec7be33d39b273823abfe1fee12f26
Cristopher <josuepaiva7@gmail.com>
2021-11-17 18:15:53 -0300

    Creacion de api para disponibilizar datos para el frontend a traves de metodos get y post, tambien la creacionde logs para la realizacion de compr

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = 3001;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+
+/*GET*/
+app.get('/products', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'ecommerce-api-master/ecommerce-api-master/product/all.json'));
+});
+
+app.get('/cart-info-2', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'ecommerce-api-master/ecommerce-api-master/cart/654.json'));
+});
+
+app.get('/categorias', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/ecommerce-api-master/ecommerce-api-master/category/all.json'));
+});
+
+app.get('/publish', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/ecommerce-api-master/ecommerce-api-master/product/publish.json'));
+});
+
+
+app.get('/categoria-info', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/ecommerce-api-master/ecommerce-api-master/category/1234.json'));
+});
+
+app.get('/cart-info', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/ecommerce-api-master/ecommerce-api-master/cart/987.json'));
+});
+
+app.get('/products-info', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/ecommerce-api-master/ecommerce-api-master/product/5678.json'));
+});
+
+app.get('/product-info-comments', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'ecommerce-api-master/ecommerce-api-master/product/5678-comments.json'));
+});
+
+app.get('/buy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ecommerce-api-master/ecommerce-api-master/cart/buy.json'));
+})
+
+
+/*POST*/
+app.post('/buy',(req, res)=>{
+  fs.writeFileSync(path.join(__dirname,'./logs/log.txt'), JSON.stringify(req.body));
+
+  let msj={
+    dato: 'Compra efectuada con exito',
+  }
+
+  res.send(JSON.stringify(msj));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
